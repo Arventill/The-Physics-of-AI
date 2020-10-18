c109c78908429d69eccac3150507d92e9be2725d
Arventilll <filip.nowicki1999@gmail.com>
2020-10-18 11:42:35 +0200

    TPA-01 Add project of the physics of AI.

---
+++ Assets/Scripts/Shell.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public GameObject explosion;
+    float mass = 10f;
+    float force = 20f;
+    float acceleration;
+    float gravity = -9.8f;
+    float gAcceleration;
+    float speedY;
+    float speedZ;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "tank")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        acceleration = force / mass;
+        speedZ += acceleration * Time.deltaTime;
+
+        gAcceleration = gravity / mass;
+        speedY += gAcceleration * Time.deltaTime;
+        //if (speedZ > 0)
+            this.transform.Translate(0, speedY, speedZ);
+
+        force -= 0.5f;
+    }
+}
---
+++ Assets/Scripts/ShellMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellMove : MonoBehaviour
+{
+    float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
